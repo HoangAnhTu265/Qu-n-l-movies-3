7f6e1dddb65d1740292973de7918d2c5f65d7c35
HoangAnhTu265 <tuhoang0237@gmail.com>
2021-09-21 11:45:09 +0700

    Initial commit

---
+++ Quản lý movies 3/Service1.svc.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace Quản_lý_movies_3
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+    public class Service1 : IService1
+    {
+        private ExamEntities1 movieService = new ExamEntities1();
+
+        public void Add(Movie m)
+        {
+            movieService.Movies.Add(m);
+            movieService.SaveChanges();
+        }
+
+        public void Delete(int Id)
+        {
+            Movie m = (from a in movieService.Movies
+                       where a.MovieId == Id
+                       select a).FirstOrDefault();
+            movieService.Movies.Remove(m);
+            movieService.SaveChanges();
+        }
+
+        public void Edit(Movie item)
+        {
+            movieService.Entry(item).State = EntityState.Modified;
+
+
+            movieService.SaveChanges();
+        }
+
+        public List<Movie> getAll()
+        {
+            return movieService.Movies.ToList();
+        }
+    }
+}
---
+++ WebApplication1/Controllers/DefaultController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        ServicesReference_Movies.Service1Client sv = new ServicesReference_Movies.Service1Client();
+        public ActionResult Index()
+        {
+            var list = sv.getAll();
+            //int a = 0;
+            //a++;
+            return View(sv.getAll());
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(ServicesReference_Movies.Movie m)
+        {
+            sv.Add(m);
+            return RedirectToAction("Index", "Default");
+        }
+
+        [HttpGet]
+        public ActionResult Edit()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Edit(ServicesReference_Movies.Movie m)
+        {
+            sv.Edit(m);
+            return RedirectToAction("Index", "Default");
+        }
+    }
+}
